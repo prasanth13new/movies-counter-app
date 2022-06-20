8f71fc4aae01e87d748a438d095ea8df921da1b1
prasanth13new <prasanth13new@gmail.com>
2022-06-20 23:12:54 +0530

    theming

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { NotFound } from './NotFound';
 import { MovieList, AddMovie } from './MovieList';
 import { MovieDetails } from './MovieDetails';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 
 export const INTIAL_MOVIE_LIST = [
@@ -95,24 +95,36 @@ export const INTIAL_MOVIE_LIST = [
   }
 ];
 
+
 function App() {
   // Lifting the state up - APP (common parent)
   const [movieList, setMovieList] = useState(INTIAL_MOVIE_LIST);
   const navigate = useNavigate();
-  
+  const [mode, setMode] = useState("light");
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <Paper elevation={4} style={{ minHeight: "100vh" }}>
+        <div className="App">
 
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit" onClick={()=> navigate("/")}>Home</Button>
-          <Button color="inherit" onClick={()=> navigate("movies")}>Movies</Button>
-          <Button color="inherit" onClick={()=> navigate("somewhere")}>Somewhere</Button>
-          <Button color="inherit" onClick={()=> navigate("/movies/add")}>AddMovie</Button>
-        </Toolbar>
-      </AppBar>
+          <AppBar position="static">
+            <Toolbar>
+              <Button color="inherit" onClick={() => navigate("/")}>Home</Button>
+              <Button color="inherit" onClick={() => navigate("movies")}>Movies</Button>
+              <Button color="inherit" onClick={() => navigate("somewhere")}>Somewhere</Button>
+              <Button color="inherit" onClick={() => navigate("/movies/add")}>AddMovie</Button>
+              <Button startIcon={mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />} color="inherit" onClick={() => setMode(mode === "dark" ? "light" : "dark")}>
+                {mode === "dark" ? "light" : "dark"} Mode
+              </Button>
+            </Toolbar>
+          </AppBar>
 
-      {/* <nav>
+          {/* <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -129,17 +141,19 @@ function App() {
         </ul>
       </nav> */}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="movies" element={<MovieList movieList={movieList} setMovieList={setMovieList} />} />
-        <Route path="/films" element={<Navigate replace to="/movies" />} />
-        <Route path="/movies/:id" element={<MovieDetails movieList={movieList} />} />
-        <Route path="/movies/add" element={<AddMovie movieList={movieList} setMovieList={setMovieList} />} />
-        <Route path="/404" element={<NotFound />} />
-        {/* /sdgsdgfhgs --> /404 */}
-        <Route path="*" element={<Navigate replace to="/404" />} />
-      </Routes>
-    </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="movies" element={<MovieList movieList={movieList} setMovieList={setMovieList} />} />
+            <Route path="/films" element={<Navigate replace to="/movies" />} />
+            <Route path="/movies/:id" element={<MovieDetails movieList={movieList} />} />
+            <Route path="/movies/add" element={<AddMovie movieList={movieList} setMovieList={setMovieList} />} />
+            <Route path="/404" element={<NotFound />} />
+            {/* /sdgsdgfhgs --> /404 */}
+            <Route path="*" element={<Navigate replace to="/404" />} />
+          </Routes>
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
